964862dd9bcf23d057e4942e9aa64257d1927997
derek <derek.oconnor@uptake.com>
2022-06-30 22:18:09 -0400

    fixed untracked files

--- venv/deploy_dash_app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcloud builds submit --tag gcr.io/xenon-crossbar-336406/dash-apps --project=xenon-crossbar-336406
-gcloud run deploy --image gcr.io/xenon-crossbar-336406/dash-apps --platform managed --project=xenon-crossbar-336406 --allow-unauthenticated
